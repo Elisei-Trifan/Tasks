dc25c94121c3b6ff9f33c632e378186b4d1365bc
Elisei-Trifan <slavaberzan@gmail.com>
2024-09-27 23:27:04 +0300

    using match()

--- exzer.js
+++ exzer.js
@@ -35,43 +35,54 @@
 // }
 // console.log(meetupsOnActivMembers(meetups))
 
-function capitalize(s, arr) {
-  return [...s]
-    .map((item, index) => (arr.includes(index) ? item.toUpperCase() : item))
-    .join('')
-}
+// function capitalize(s, arr) {
+//   return [...s]
+//     .map((item, index) => (arr.includes(index) ? item.toUpperCase() : item))
+//     .join('')
+// }
 
-console.log(capitalize('abcdscadzef', [1, 2, 5]))
+// console.log(capitalize('abcdscadzef', [1, 2, 5]))
 
-function friend(friends) {
-  return friends
-    .map((item) => (item.length === 4 ? item : ''))
-    .filter((item) => item != '')
-}
+// function friend(friends) {
+//   return friends
+//     .map((item) => (item.length === 4 ? item : ''))
+//     .filter((item) => item != '')
+// }
 
-console.log(friend(['Ryan', 'Kieran', 'Mark']))
+// console.log(friend(['Ryan', 'Kieran', 'Mark']))
 
-function accum(s) {
-  return [...s]
-    .map((item, index) => item.toUpperCase() + item.repeat(index).toLowerCase())
-    .join('-')
-}
+// function accum(s) {
+//   return [...s]
+//     .map((item, index) => item.toUpperCase() + item.repeat(index).toLowerCase())
+//     .join('-')
+// }
 
-console.log(accum('djmsun'))
+// console.log(accum('djmsun'))
 
-function digitalRoot(n) {
-  a = n
-    .toString()
-    .split('')
-    .map(Number)
-    .reduce((acc, item) => {
-      return acc + item
-    }, 0)
-  if (a < 10) {
-    return a
-  } else if (a >= 10) {
-    return digitalRoot(a)
-  }
-}
+// function digitalRoot(n) {
+//   a = n
+//     .toString()
+//     .split('')
+//     .map(Number)
+//     .reduce((acc, item) => {
+//       return acc + item
+//     }, 0)
+//   if (a < 10) {
+//     return a
+//   } else if (a >= 10) {
+//     return digitalRoot(a)
+//   }
+// }
 
-console.log(digitalRoot(49))
+// console.log(digitalRoot(49))
+
+// function getCount(str) {
+//   m = str.match(/[aoiue]/gi)
+//   return m === null ? 0 : m.length
+// }
+
+// console.log(getCount('asfwrooodgw'))
+
+console.log('helloaAa'.match(/[ao]/gi).length)
+
+console.log('a aa aaa aaaa'.split(' '))
