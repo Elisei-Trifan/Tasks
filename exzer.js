927d254823c59b4d5360784e709bd382474673b1
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-04 12:51:31 +0300

    Geometry Basics: Circle Area in 2D

--- exzer.js
+++ exzer.js
@@ -270,18 +270,36 @@
 // "scissors", "rock" --> "Player 2 won!"
 // "paper", "paper" --> "Draw!"
 
-const rps = (p1, p2) => {
-  if (p1 === p2) {
-    return 'Draw!'
-  }
+// const rps = (p1, p2) => {
+//   if (p1 === p2) {
+//     return 'Draw!'
+//   }
 
-  if (
-    (p1 === 'rock' && p2 === 'scissors') ||
-    (p1 === 'scissors' && p2 === 'paper') ||
-    (p1 === 'paper' && p2 === 'rock')
-  ) {
-    return 'Player 1 won!'
-  } else {
-    return 'Player 2 won!'
+//   if (
+//     (p1 === 'rock' && p2 === 'scissors') ||
+//     (p1 === 'scissors' && p2 === 'paper') ||
+//     (p1 === 'paper' && p2 === 'rock')
+//   ) {
+//     return 'Player 1 won!'
+//   } else {
+//     return 'Player 2 won!'
+//   }
+// }
+
+class Circle {
+  constructor(center, radius) {
+    this.center = center
+    this.radius = radius
   }
+}
+
+class Point {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+}
+
+function circleArea(circle) {
+  return Math.PI * Math.pow(circle.radius, 2)
 }
