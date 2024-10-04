5e83d768c84a7400db2c7d94366b704d43eff122
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-04 12:30:16 +0300

    Rock Paper Scissors!

--- exzer.js
+++ exzer.js
@@ -258,10 +258,30 @@
 //   return count
 // }
 
-function testEven(n) {
-  if (n % 2 === 0) {
-    return true
+// function testEven(n) {
+//   if (n % 2 === 0) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+const rps = (p1, p2) => {
+  if (p1 === p2) {
+    return 'Draw!'
+  }
+
+  if (
+    (p1 === 'rock' && p2 === 'scissors') ||
+    (p1 === 'scissors' && p2 === 'paper') ||
+    (p1 === 'paper' && p2 === 'rock')
+  ) {
+    return 'Player 1 won!'
   } else {
-    return false
+    return 'Player 2 won!'
   }
 }
