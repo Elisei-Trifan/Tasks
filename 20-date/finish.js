4756277e1a365fdc680ea0b0654da9cb31ca3c3d
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-16 16:58:36 +0300

    date in ms

--- 20-date/finish.js
+++ 20-date/finish.js
@@ -4,3 +4,23 @@
  *
  * 2. Выведите значение в консоль
  */
+
+/*const date = new Date()
+const dateMs = date.getTime()
+console.log(dateMs)*/
+
+let start, end
+
+// const array = [1, 2]
+// array.forEach((element) => {
+//   return console.log(element)
+// })
+
+// end = new Date()
+
+start = new Date()
+for (let i = 1; i < 5; i++) {
+  console.log(i)
+}
+end = new Date()
+console.log(end.getTime() - start.getTime())
